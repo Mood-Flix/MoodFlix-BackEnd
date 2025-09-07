f096c1845b97bb2326e59f52e0bfeb9f5e17eb2e
준영킴 <zvpdrnls@gmail.com>
2025-09-07 17:14:35 +0900

    fix: MovieKeywordRepository.findKeywordIdsByMovieId 추가

--- src/main/java/com/duck/moodflix/movie/repository/MovieKeywordRepository.java
+++ src/main/java/com/duck/moodflix/movie/repository/MovieKeywordRepository.java
@@ -2,10 +2,15 @@ package com.duck.moodflix.movie.repository;
 
 import com.duck.moodflix.movie.domain.entity.MovieKeyword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+import java.util.Set;
 
 public interface MovieKeywordRepository extends JpaRepository<MovieKeyword, Long> {
-    List<MovieKeyword> findByMovieId(Long movieId);
+
     boolean existsByMovieIdAndKeywordId(Long movieId, Long keywordId);
+
+    @Query("select mk.keyword.id from MovieKeyword mk where mk.movie.id = :movieId")
+    Set<Long> findKeywordIdsByMovieId(@Param("movieId") Long movieId);
 }
