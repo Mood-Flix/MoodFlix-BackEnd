03ff1f16109f81117ec4a12dc058c4831463cce0
준영킴 <zvpdrnls@gmail.com>
2025-10-16 18:55:01 +0900

    fix: 요청에 movieId 추가 및 엔티티 수정

--- src/main/java/com/duck/moodflix/calendar/dto/CalendarDtos.java
+++ src/main/java/com/duck/moodflix/calendar/dto/CalendarDtos.java
@@ -1,5 +1,7 @@
 package com.duck.moodflix.calendar.dto;
 
+import com.duck.moodflix.movie.dto.response.MovieSummaryResponse;
+
 import java.time.LocalDate;
 import java.util.List;
 
@@ -8,7 +10,8 @@ public class CalendarDtos {
     public record EntryRequest(
             LocalDate date,      // 캘린더 날짜
             String moodEmoji,    // 기분 이모지
-            String note          // 사용자 메모
+            String note,          // 사용자 메모
+            Long movieId          //[프론트 추천 선택] 영화 ID (선택)
     ) {}
 
     public record RecommendationResponse(
@@ -24,6 +27,7 @@ public class CalendarDtos {
             LocalDate date,
             String note,
             String moodEmoji,
+            MovieSummaryResponse selectedMovie, // 선택된 영화 정보
             List<RecommendationResponse> recommendations
     ) {}
 }
